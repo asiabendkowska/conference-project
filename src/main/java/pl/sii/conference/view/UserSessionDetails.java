8a7b5ebf19bd6afbd167c07f76c8d1ab2f487da2
asia <asiabendkowska@gmail.com>
2019-05-15 00:23:29 +0200

    User added to session details.

--- src/main/java/pl/sii/conference/view/UserSessionDetails.java
+++ src/main/java/pl/sii/conference/view/UserSessionDetails.java
@@ -3,11 +3,12 @@ package pl.sii.conference.view;
 import lombok.Data;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.SessionScope;
+import pl.sii.conference.domain.model.User;
 
 @Component
 @SessionScope
 @Data
 public class UserSessionDetails {
     private boolean loggedIn = false;
-    private String login;
+    private User user;
 }
