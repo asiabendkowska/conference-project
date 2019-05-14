06794524cfbce3a19b383427e91b22ebbb01b616
asia <asiabendkowska@gmail.com>
2019-05-15 00:27:07 +0200

    LogIn and registration functionality added to UserService.

--- src/main/java/pl/sii/conference/service/UserService.java
+++ src/main/java/pl/sii/conference/service/UserService.java
@@ -2,6 +2,9 @@ package pl.sii.conference.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import pl.sii.conference.exception.DuplicateLoginRegistrationException;
+import pl.sii.conference.exception.DuplicateUserRegistrationException;
+import pl.sii.conference.exception.UserNotFoundException;
 import pl.sii.conference.view.UserSessionDetails;
 import pl.sii.conference.domain.model.User;
 import pl.sii.conference.domain.repository.UserRepository;
@@ -15,31 +18,36 @@ public class UserService {
     private final UserRepository userRepository;
     private final UserSessionDetails userSessionDetails;
 
-    public void userLogIn(String login, String email) {
-        if (checkIfUserExists(login, email)) {
-            userSessionDetails.setLoggedIn(true);
-            userSessionDetails.setLogin(login);
-        } else {
-            //TODO: change into notification
-            throw new RuntimeException("You need to register first!!!");
-        }
+    public void userLogIn(String login, String email) throws UserNotFoundException {
+        User user = getUser(login, email).orElseThrow(() -> new UserNotFoundException("You need to register first!"));
+        userSessionDetails.setLoggedIn(true);
+        userSessionDetails.setUser(user);
     }
 
-    public void registerUser(String login, String email) {
-        if(!checkIfUserExists(login, email)) {
+    public void registerUser(String login, String email) throws DuplicateUserRegistrationException, DuplicateLoginRegistrationException {
+        if (checkIfLoginExists(login)){
+            String message;
+            if (checkIfUserExists(login, email)) {
+                //TODO: is this if really needed?
+                throw new DuplicateUserRegistrationException("User already exists!");
+            } else {
+                throw new DuplicateLoginRegistrationException("Inserted login is already taken!");
+            }
+        } else {
+            addUser(login, email);
+        }
+
+        /*if(!checkIfUserExists(login, email)) {
             addUser(login, email);
         } else if (checkIfLoginExists(login)) {
             //TODO: change into notification
-            throw new RuntimeException("Inserted login is already taken!!!");
+            Notification.show("Inserted login is already taken!");
+            //throw new RuntimeException("Inserted login is already taken!");
         } else {
             //TODO: change into notification
-            throw new RuntimeException("User already exists!!!");
-        }
-    }
-
-    boolean checkIfLoginExists(String login) {
-        Optional<User> user = userRepository.findUserByLogin(login);
-        return user.isPresent();
+            Notification.show("User already exists!");
+            //throw new RuntimeException("User already exists!");
+        }*/
     }
 
     void addUser(String login, String email) {
@@ -47,6 +55,15 @@ public class UserService {
         user.setLogin(login);
         user.setEmail(email);
         userRepository.save(user);
+    }
+
+    Optional<User> getUser(String login, String email) {
+        return userRepository.findUserByLoginAndEmail(login, email);
+    }
+
+    boolean checkIfLoginExists(String login) {
+        Optional<User> user = userRepository.findUserByLogin(login);
+        return user.isPresent();
     }
 
     boolean checkIfUserExists(String login, String email) {
