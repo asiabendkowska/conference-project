ffe0663a4546b63a051b635ed11a1022565d488c
asia <asiabendkowska@gmail.com>
2019-05-15 22:39:30 +0200

    EmailService modified.

--- src/main/java/pl/sii/conference/service/EmailService.java
+++ src/main/java/pl/sii/conference/service/EmailService.java
@@ -8,13 +8,14 @@ import java.time.LocalDateTime;
 
 @Service
 public class EmailService {
-    private static final String filePath = "resources\\static\\confirmation.txt";
-    private static final String reservationMessage = "Confirmation of reservation for lecture: ";
+    private static final String FILE_PATH = "resources\\static\\confirmation.txt";
+    private static final String RESERVATION_MESSAGE = "Confirmation of reservation for lecture: ";
+    //TODO: create file if not exists
 
     void sendConfirmation(String email, String lectureTitle) throws FileNotFoundException {
-        PrintWriter writeToFile = new PrintWriter(filePath);
+        PrintWriter writeToFile = new PrintWriter(FILE_PATH);
         LocalDateTime localDateTime = LocalDateTime.now();
-        writeToFile.println(localDateTime + " " + email + " " + reservationMessage + lectureTitle);
+        writeToFile.println(localDateTime + " " + email + " " + RESERVATION_MESSAGE + lectureTitle);
         writeToFile.close();
     }
 }
