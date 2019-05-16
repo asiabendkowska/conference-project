67f82be623f4e4671cea1f2c67414d8ae458f8e9
asia <asiabendkowska@gmail.com>
2019-05-16 13:01:02 +0200

    removed email service constructor

--- src/main/java/pl/sii/conference/service/EmailService.java
+++ src/main/java/pl/sii/conference/service/EmailService.java
@@ -10,17 +10,12 @@ public class EmailService {
     private static final String FILE_PATH = "src/main/resources/confirmation.txt";
     private static final String RESERVATION_MESSAGE = "Confirmation of reservation for lecture: ";
 
-    EmailService() throws IOException {
-        File file = new File(FILE_PATH);
-        if (!file.exists()) {
-            file.createNewFile();
-        }
-    }
+
 
     void sendConfirmation(String email, String lectureTitle) throws IOException {
         LocalDateTime now = LocalDateTime.now();
-        try (PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter(FILE_PATH, true)))) {
-            writer.println(now + " " + email + " " + RESERVATION_MESSAGE + lectureTitle);
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(FILE_PATH, true))) {
+            writer.write(now + " " + email + " " + RESERVATION_MESSAGE + lectureTitle + "\n");
         }
     }
 }
